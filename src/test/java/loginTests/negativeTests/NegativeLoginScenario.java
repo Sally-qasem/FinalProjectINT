27117b49110f281dd25d68b4bbb91ccc5fd65723
Sally-qasem <sallyqasem96@gmail.com>
2024-04-01 20:25:59 +0300

    NegativeLoginScenariosUpdated

--- src/test/java/loginTests/negativeTests/NegativeLoginScenario.java
+++ src/test/java/loginTests/negativeTests/NegativeLoginScenario.java
@@ -6,13 +6,16 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.Assert;
 
 public class NegativeLoginScenario {
     @Test(dataProvider = "NegativeLoginScenario")
-    public void loginTest(String username, String password) {
-        System.out.println(username + "- " + password);
+    public void loginTest(String username, String password, String expectedError) {
+        System.out.println(username +  "- " + password + " " +expectedError);
+
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com/");
+
         WebElement userName = driver.findElement(By.id("user-name"));
         WebElement Password = driver.findElement(By.id("password"));
         WebElement loginButton = driver.findElement(By.id("login-button"));
@@ -20,30 +23,25 @@ public class NegativeLoginScenario {
         userName.clear();
         Password.clear();
 
-        String usernames = username;
         userName.sendKeys(username);
-        String pass = password;
-        Password.sendKeys(pass);
+        Password.sendKeys(password);
 
         loginButton.click();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        driver.navigate().refresh();
-        driver.quit();
-
-//        WebElement errorMessage = driver.findElement(By.cssSelector("#login_button_container > div > form > div.error-message-container.error > h3"));
-//        String error = errorMessage.getText();
-//        Assert.assertEquals(error, errorMessage.getText());
+        WebElement errorMessageElement = driver.findElement(By.cssSelector("#login_button_container > div > form > div.error-message-container.error > h3"));
+        String actualError = errorMessageElement.getText();
+        Assert.assertEquals(actualError, expectedError);
     }
-
     @DataProvider(name = "NegativeLoginScenario")
     public Object[][] getData()
     {
-        Object[][] data = {{"standard_user","incorrect_pwd"},{"incorrect_user","secret_sauce"},{"incorrect_user","incorrect_pwd"},{"","secret_sauce"},{"standard_user",""},{"",""}};
+        Object[][] data = {
+                {"standard_user","incorrect_pwd", "Epic sadface: Username and password do not match any user in this service"},
+                {"incorrect_user","secret_sauce", "Epic sadface: Username and password do not match any user in this service"},
+                {"incorrect_user","incorrect_pwd", "Epic sadface: Username and password do not match any user in this service"},
+                {"","secret_sauce", "Epic sadface: Username is required"},
+                {"standard_user","", "Epic sadface: Password is required"},
+                {"","", "Epic sadface: Username is required"}
+        };
         return data;
     }
 }
-
